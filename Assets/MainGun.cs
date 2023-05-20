d55e41b321a17d66b2ff3ec53ad42e4512cf2026
Crashington <brueggemann.florian@web.de>
2023-05-20 02:38:09 +0200

    Player Movement & Turret Aim Completed

--- Assets/MainGun.cs
+++ Assets/MainGun.cs
@@ -34,11 +34,13 @@ public class MainGun : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        aimDirection = Vector2.MoveTowards(aimDirection, aimInput, Time.fixedDeltaTime * stats.aimSpeed);
+        aimDirection = Vector3.RotateTowards(aimDirection, aimInput, Time.fixedDeltaTime * stats.aimSpeed, 1);
         
         
         //mainGun.transform.rotation = Quaternion.Euler(aimDirection.x, aimDirection.y, 0);
         mainGun.transform.right = aimDirection - new Vector2(mainGun.transform.position.x, mainGun.transform.position.y) ;
+
+        mainGun.transform.eulerAngles = new Vector3(mainGun.transform.eulerAngles.x, mainGun.transform.eulerAngles.y, mainGun.transform.eulerAngles.z - 90);
 
     }
 }

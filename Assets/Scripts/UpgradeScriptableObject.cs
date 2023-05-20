c5069d7fa4d84158b9fe056bbd8244ce255d73ea
Leon <leon@nyoucomer.de>
2023-05-20 22:08:07 +0200

    Made working shop whoop whoop

---
+++ Assets/Scripts/UpgradeScriptableObject.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Upgrade", menuName = "ScriptableObjects/Upgrade", order = 1)]
+public class UpgradeScriptableObject : ScriptableObject
+{
+    public string upgradeName;
+    public bool infiniteUpgrades;
+    public List<int> costs;
+}
